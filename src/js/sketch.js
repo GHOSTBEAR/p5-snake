e3a6a86350924e2fa5bd0f717fb089552fe25ea1
Andreas E <andreas.everos@gmail.com>
2016-12-23 19:17:18 +0100

    If (score inside body) move it;

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,114 +1,126 @@
-var snake, score, sizes, highscores , person = "NUL", dir = 2, scr = 1;
+var snake, score, sizes, highscores, person = "NUL", dir = 2, scr = 1;
 
 window.addEventListener("keydown", function(e) {
     // space and arrow keys
-    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
         e.preventDefault();
     }
 }, false);
 
 function setup() {
-	var config = {
-		apiKey: "AIzaSyDLQWDljQw2ZBk4aTwb0kDoqCU7ut5PCU8",
-		authDomain: "snake-game-highscore.firebaseapp.com",
-		databaseURL: "https://snake-game-highscore.firebaseio.com",
-		storageBucket: "snake-game-highscore.appspot.com",
-		messagingSenderId: "697454527983"
-	};
+    var config = {
+        apiKey: "AIzaSyDLQWDljQw2ZBk4aTwb0kDoqCU7ut5PCU8",
+        authDomain: "snake-game-highscore.firebaseapp.com",
+        databaseURL: "https://snake-game-highscore.firebaseio.com",
+        storageBucket: "snake-game-highscore.appspot.com",
+        messagingSenderId: "697454527983"
+    };
 
-	firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
-	var database = firebase.database();
-	highscores = database.ref('highscores');
+    var database = firebase.database();
+    highscores = database.ref('highscores');
 
-	// Prompt user to enter name
-	person = prompt("Enter a three letter name", "AAA");
-	if (person != null && person.length <= 3 && person.length != "") {
-		console.log("Good to go");
-	} else {
-		console.log("-_-");
-		person = "CPU";
-	}
+    // Prompt user to enter name
+    person = prompt("Enter a three letter name", "AAA");
+    if (person != null && person.length <= 3 && person.length != "") {
+        console.log("Good to go");
+    } else {
+        console.log("-_-");
+        person = "CPU";
+    }
 
-	// Sets up p5.js canvas and framerate
-	var canvas = createCanvas(501, 501);
-	canvas.parent("game");
-	frameRate(7);
-	sizes = 20; // Size of snake
+    // Sets up p5.js canvas and framerate
+    var canvas = createCanvas(501, 501);
+    canvas.parent("game");
+    frameRate(7);
+    sizes = 20; // Size of snake
 
-	// Initialize
-	snake = new Snake();
-	score = new Score();
+    // Initialize
+    snake = new Snake();
+    score = new Score();
 
-	// Gets database from Firebase
-	var ref = database.ref("highscores");
-	ref.on("value", gotData, errData);
+    // Gets database from Firebase
+    var ref = database.ref("highscores");
+    ref.on("value", gotData, errData);
 
-	// Puts local high score into html file
-	var localName = localStorage.getItem("name");
-	var localHighScore = localStorage.getItem("highscore");
-	if (localStorage.getItem("highscore") != null) {
-		document.getElementById("highscore").innerHTML = "Local: " + localName + " " + localHighScore;
-	}
+    // Puts local high score into html file
+    var localName = localStorage.getItem("name");
+    var localHighScore = localStorage.getItem("highscore");
+    if (localStorage.getItem("highscore") != null) {
+        document.getElementById("highscore").innerHTML = "Local: " + localName + " " + localHighScore;
+    }
 }
 
 function draw() {
-	background(10);
-	score.show();
-	snake.update(dir); 
-	snake.show(); 
-	snake.check();
+    background(10);
+    score.show();
+    snake.update(dir);
+    snake.show();
+    snake.check();
+    snake.scoreinbody(score);
 
-	// Checks if snake is on score
-	var playerScore = document.getElementById("score");
-	if (snake.x === score.x && snake.y === score.y) 
-	 snake.body.push(""), score.move(), scr++, playerScore.innerHTML = "Score: " + scr;
+    // Checks if snake is on score
+    var playerScore = document.getElementById("score");
+    if (snake.x === score.x && snake.y === score.y)
+        snake.body.push(""), score.move(), scr++, playerScore.innerHTML = "Score: " + scr;
 }
 
 function keyPressed() {
-	keyCode === LEFT_ARROW ? dir = 1 : keyCode === RIGHT_ARROW && (dir = 3);
-	keyCode === UP_ARROW ? dir = 2 : keyCode === DOWN_ARROW && (dir = 4);
-	keyCode === 65 ? snake.body.push("") : keyCode === 66 && (scr++);
-	keyCode === 80 && noLoop();
+    if (keyCode === LEFT_ARROW)
+        dir = 1;
+    if (keyCode === RIGHT_ARROW)
+        dir = 3;
+    if (keyCode === UP_ARROW)
+        dir = 2;
+    if (keyCode === DOWN_ARROW)
+        dir = 4;
+    if (keyCode === 65)
+        snake.body.push("");
+    if (keyCode === 66)
+        score.move();
+    if (keyCode === 80)
+        noLoop();
 }
 
 function gotData(data) {
-	var highscores = data.val();
-	// Grab the keys to iterate over the object
-	var keys = Object.keys(highscores);
+    var highscores = data.val();
+    // Grab the keys to iterate over the object
+    var keys = Object.keys(highscores);
 
-	var high = 0, name = "ABC";
+    var high = 0,
+        name = "ABC";
 
-	for (var i = 0; i < keys.length; i++) {
-		var k = keys[i];
-		// Checks which of the highscores is the highest highscore
-		if (highscores[k].score > high) {
-			high = highscores[k].score, name = highscores[k].name;
-		}
-	}
+    for (var i = 0; i < keys.length; i++) {
+        var k = keys[i];
+        // Checks which of the highscores is the highest highscore
+        if (highscores[k].score > high) {
+            high = highscores[k].score, name = highscores[k].name;
+        }
+    }
 
-	document.getElementById("globalhighscore").innerHTML = "Global: " + name + " " + high;
+    document.getElementById("globalhighscore").innerHTML = "Global: " + name + " " + high;
 }
 
 function errData(err) {
-	console.log("Error");
-	console.log(err);
+    console.log("Error");
+    console.log(err);
 }
 
 var reset = document.getElementById("reset");
-reset.addEventListener("click", function(){
+reset.addEventListener("click", function() {
     snake = new Snake();
-	score.move();
-	scr = 0;
-	loop();
+    score.move();
+    scr = 0;
+    loop();
 });
 
 var changeName = document.getElementById("changeName");
 changeName.addEventListener("click", function() {
-	person = prompt("Enter a three letter name", "AAA");
-	if (person != null && person.length <= 3 && person.length != "") {
-		console.log("Changed Name");
-	} else {
-		person = "CPU";
-	}
+    person = prompt("Enter a three letter name", "AAA");
+    if (person != null && person.length <= 3 && person.length != "") {
+        console.log("Changed Name");
+    } else {
+        person = "CPU";
+    }
 });
--- src/js/snake.js
+++ src/js/snake.js
@@ -1,49 +1,68 @@
 function Snake() {
-	this.x = Math.floor(width / 2 / 20) * 20;
-	this.y = Math.floor(height / 2 / 20) * 20;
-	this.body = [];
-	this.olds = [[], []];
+    this.x = Math.floor(width / 2 / 20) * 20;
+    this.y = Math.floor(height / 2 / 20) * 20;
+    this.body = [];
+    this.olds = [
+        [],
+        []
+    ];
 
-	this.update = function (dir) {
-		dir == 1 ? this.x -= sizes : dir == 2 && (this.y -= sizes);
-		dir == 3 ? this.x += sizes : dir == 4 && (this.y += sizes);
-	};
+    this.update = function(dir) {
+        if (dir == 1)
+            this.x -= sizes;
+        if (dir == 2)
+            this.y -= sizes;
+        if (dir == 3)
+            this.x += sizes;
+        if (dir == 4)
+            this.y += sizes;
+        this.olds[0].unshift(this.x), this.olds[1].unshift(this.y);
+    };
 
-	this.show = function () {
-		fill(0, 255, 0), stroke(50);
-		rect(this.x, this.y, sizes, sizes);
-		this.olds[0].unshift(this.x), this.olds[1].unshift(this.y);
-		for (var i = 0; i < this.body.length; i++) {
-			rect(this.olds[0][i + 1], this.olds[1][i + 1], sizes, sizes);
-		}
-	};
+    this.show = function() {
+        // Head
+        fill(0, 200, 0), stroke(50);
+        rect(this.x, this.y, sizes, sizes);
+        // Body
+        for (var i = 0; i < this.body.length; i++) {
+            fill(0, 255, 0), stroke(50);
+            rect(this.olds[0][i + 1], this.olds[1][i + 1], sizes, sizes);
+        }
+    };
 
-	this.check = function () {
-		if (this.x < 0 || this.y < 0 || this.x >= width || this.y >= height)
-			this.death("Out of bounds");
-		for (var i = 0; i < this.body.length; i++) {
-			if (this.olds[0][i + 1] === this.x && this.olds[1][i + 1] === this.y)
-				this.death("Killed in action");
-		}
-	}
+    this.check = function() {
+        if (this.x < 0 || this.y < 0 || this.x >= width - 5 || this.y >= height - 5)
+            this.death("Out of bounds");
+        for (var i = 0; i < this.body.length; i++) {
+            if (this.olds[0][i + 1] === this.x && this.olds[1][i + 1] === this.y)
+                this.death("Killed in action");
+        }
+    }
 
-	this.death = function (reason) {
-		alert(reason);
-		noLoop();
-		this.sumbitdata();
-	}
+    this.scoreinbody = function(score) {
+        for (var i = 0; i < this.body.length; i++) {
+            if (this.olds[0][i + 1] === score.x && this.olds[1][i + 1] === score.y)
+                score.move();
+        }
+    }
 
-	this.sumbitdata = function () {
-		var data = {
-			name: person,
-			score: scr
-		};
-		highscores.push(data, this.finished);
-		if (scr > localStorage.getItem("highscore"))
-	 		(localStorage.setItem("highscore", scr), localStorage.setItem("name", person));
-	}
-	
-	this.finished = function(error) {
-		error ? console.log('ooops') : console.log('data saved!');
-	}
+    this.death = function(reason) {
+        alert(reason);
+        noLoop();
+        this.sumbitdata();
+    }
+
+    this.sumbitdata = function() {
+        var data = {
+            name: person,
+            score: scr
+        };
+        highscores.push(data, this.finished);
+        if (scr > localStorage.getItem("highscore"))
+            (localStorage.setItem("highscore", scr), localStorage.setItem("name", person));
+    }
+
+    this.finished = function(error) {
+        error ? console.log('ooops') : console.log('data saved!');
+    }
 }
